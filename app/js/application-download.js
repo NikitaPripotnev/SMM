ac8a9c6ceead2fb8da9137b1499e83f94f78bebf
Nikita Pripotnev <nikita1996-13@mail.ru>
2019-02-05 17:52:41 +0700

    added js

---
+++ app/js/application-download.js
@@ -0,0 +1,16 @@
+const downloadData = url => {
+  return new Promise(function(resolved, rejected) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.addEventListener('load', function() {
+      if (xhr.status < 400) resolved(JSON.parse(xhr.responseText));
+      else rejected(new Error('Request failed: ' + xhr.statusText));
+    });
+    xhr.addEventListener('error', function() {
+      rejected(new Error('Network error'));
+    });
+    xhr.send();
+  });
+};
+
+export default downloadData;
