918cf127cba6e92c3c086633bf692cfc7a96f60f
Egor Tsyplakov <tsyplakov.ee@gmail.com>
2019-03-01 20:11:11 +0700

    Added auth for proxy

--- telegramproxy/src/main/java/smm/agency/infra/BotOptionsConfiguration.java
+++ telegramproxy/src/main/java/smm/agency/infra/BotOptionsConfiguration.java
@@ -7,18 +7,30 @@ import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.bots.DefaultBotOptions;
 import org.telegram.telegrambots.meta.ApiContext;
 
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+
 @Configuration
 @Slf4j
 public class BotOptionsConfiguration {
 
     @Bean
     public DefaultBotOptions botOptionsWithProxy(@Value("${proxy.host}") String proxyHost,
-                                                 @Value("${proxy.port}") int proxyPort) {
+                                                 @Value("${proxy.port}") int proxyPort,
+                                                 @Value("${proxy.login}") String proxyLogin,
+                                                 @Value("${proxy.password}") String proxyPassword) {
+
+        Authenticator.setDefault(new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(proxyLogin, proxyPassword.toCharArray());
+            }
+        });
+
         DefaultBotOptions botOptions = ApiContext.getInstance(DefaultBotOptions.class);
 
         botOptions.setProxyHost(proxyHost);
         botOptions.setProxyPort(proxyPort);
-        botOptions.setProxyType(DefaultBotOptions.ProxyType.HTTP);
+        botOptions.setProxyType(DefaultBotOptions.ProxyType.SOCKS5);
 
         return botOptions;
     }
